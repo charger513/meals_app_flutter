828a64a711a244a7541dddc086ec93a421e01222
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-23 13:26:51 -0500

    Passing Data via the Constructor

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:meals_app_flutter/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem({this.title, this.color});
+  CategoryItem({this.id, this.title, this.color});
 
   void _selectCategory(BuildContext context) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (_) => CategoryMealsScreen()));
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) => CategoryMealsScreen(
+        categoryId: id,
+        categoryTitle: title,
+      ),
+    ));
   }
 
   @override
