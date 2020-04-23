13208861b6d174c9b2ed33eaffa60711fa02f6a3
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-23 13:20:19 -0500

    Navigating to a New Page

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app_flutter/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,21 +7,33 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem({this.title, this.color});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (_) => CategoryMealsScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(
+          15), // Should match with the Border Radius of the Container child
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title, style: Theme.of(context).textTheme.title),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(
+              15), // Should match with the Border Radius of the InkWell
         ),
-        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
